e711a37759ccac2e02ef5ac20121c013ece5f70e
ziegler <ziegler@jlab.org>
2019-11-15 13:55:09 -0500

    Small code re-structuring.

--- reconstruction/tof/src/main/java/org/jlab/rec/tof/cluster/ClusterFinder.java
+++ reconstruction/tof/src/main/java/org/jlab/rec/tof/cluster/ClusterFinder.java
@@ -14,6 +14,7 @@ import org.jlab.rec.tof.hit.ctof.Hit;
 /**
  *
  * @author ziegler
+ * @author Adam 
  *
  */
 public class ClusterFinder {
@@ -21,69 +22,77 @@ public class ClusterFinder {
     private double cluster_size_ = 1;
     
     public ClusterFinder() {
-        // TODO Auto-generated constructor stub
+        clusters_hitenergy     = new ArrayList<Double>();
+        clusters_x             = new ArrayList<Double>();
+        clusters_y             = new ArrayList<Double>();
+        clusters_z             = new ArrayList<Double>();
+        clusters_time          = new ArrayList<Double>();
+        clusters_ID            = new ArrayList<Integer>();
+        clusters_ID_spectator  = new ArrayList<Integer>();
+        clusters_sector        = new ArrayList<Integer>();
+        clusters_panel         = new ArrayList<Integer>();
+        clusters_dummy         = new ArrayList<Integer>();           
     }
     
+    ArrayList<Double>  clusters_hitenergy;
+    ArrayList<Double>  clusters_x;
+    ArrayList<Double>  clusters_y;
+    ArrayList<Double>  clusters_z;
+    ArrayList<Double>  clusters_time;
+    ArrayList<Integer> clusters_ID;
+    ArrayList<Integer> clusters_ID_spectator; //define:
+    ArrayList<Integer> clusters_sector;
+    ArrayList<Integer> clusters_panel;
+    ArrayList<Integer> clusters_dummy; //define:
+    
     /**
-     * int panel, int sector, int paddle
+     * 
+     * @param hits2 list of hits
+     * @return list of clusters
      */
     public ArrayList<Cluster> findClusters(List<?> hits2) {
         ArrayList<Cluster> clusters = new ArrayList<Cluster>();
         if (hits2 != null) {
             
+            clusters_hitenergy.clear();
+            clusters_x.clear();
+            clusters_y.clear();
+            clusters_z.clear();
+            clusters_time.clear();
+            clusters_ID.clear();
+            clusters_ID_spectator.clear();
+            clusters_sector.clear();
+            clusters_panel.clear();
+            clusters_dummy.clear();
             
             int cid = 1; // cluster id, will increment with each new good cluster
-            int size = 0;
-            
-            ArrayList<Double>  clusters_hitenergy;
-            ArrayList<Double>  clusters_x;
-            ArrayList<Double>  clusters_y;
-            ArrayList<Double>  clusters_z;
-            ArrayList<Double>  clusters_time;
-            ArrayList<Integer> clusters_ID;
-            ArrayList<Integer> clusters_ID_spectator;
-            ArrayList<Integer> clusters_sector;
-            ArrayList<Integer> clusters_panel;
-            ArrayList<Integer> clusters_dummy;
-            
-            clusters_hitenergy     = new ArrayList<Double>();
-            clusters_x             = new ArrayList<Double>();
-            clusters_y             = new ArrayList<Double>();
-            clusters_z             = new ArrayList<Double>();
-            clusters_time          = new ArrayList<Double>();
-            clusters_ID            = new ArrayList<Integer>();
-            clusters_ID_spectator  = new ArrayList<Integer>();
-            clusters_sector        = new ArrayList<Integer>();
-            clusters_panel         = new ArrayList<Integer>();
-            clusters_dummy         = new ArrayList<Integer>();
-            
             
             //array of hits ID
             ArrayList<ArrayList<Integer>> clusters_ID_array = new ArrayList<ArrayList<Integer>>();
             
-            
-            
-            size = hits2.size();
-            
-            
-            
+            //minimum energy cut to pass the cluster
             double minEn=(hits2.get(0) instanceof org.jlab.rec.tof.hit.ftof.Hit)?
             org.jlab.rec.ftof.Constants.MINENERGY:org.jlab.rec.ctof.Constants.MINENERGY;
             
-            for (int i = 0; i < size; i++) {
-                
+            for (int i = 0; i < hits2.size(); i++) {
                 if (((AHit) hits2.get(i)).get_Energy() < minEn) {
                     continue;
                 }
-                
+                //if(hits2.get(0) instanceof org.jlab.rec.tof.hit.ctof.Hit)
+                //    System.out.println("passed hit :"+(((AHit) hits2.get(i)).get_Id())+", "
+                //        +(((AHit) hits2.get(i)).get_Panel())+ " position "+((AHit) hits2.get(i)).get_Position().toString());
                 // using the unit cm instead of mm, so divided by 10
+                // the units are already in cm ?
                 clusters_hitenergy.add (((AHit) hits2.get(i)).get_Energy());
                 clusters_x.add         (((AHit) hits2.get(i)).get_Position().x() /10.0);
                 clusters_y.add         (((AHit) hits2.get(i)).get_Position().y() /10.0);
                 clusters_z.add         (((AHit) hits2.get(i)).get_Position().z() /10.0);
+                //clusters_x.add         (((AHit) hits2.get(i)).get_Position().x());
+                //clusters_y.add         (((AHit) hits2.get(i)).get_Position().y());
+                //clusters_z.add         (((AHit) hits2.get(i)).get_Position().z());
                 clusters_time.add      (((AHit) hits2.get(i)).get_t());
                 clusters_ID.add        (((AHit) hits2.get(i)).get_Id());
-                clusters_ID_spectator.add        (((AHit) hits2.get(i)).get_Id());
+                clusters_ID_spectator.add(((AHit) hits2.get(i)).get_Id());
                 clusters_sector.add    (((AHit) hits2.get(i)).get_Sector());
                 clusters_panel.add     (((AHit) hits2.get(i)).get_Panel());
                 
@@ -106,10 +115,15 @@ public class ClusterFinder {
                 
             }
             else if(clusters_x.size()==2){
-                double distance = sqrt( (clusters_x.get(0)-clusters_x.get(1))*(clusters_x.get(0)-clusters_x.get(1))/sigmaX(clusters_x.get(0))/sigmaX(clusters_x.get(1))
-                                       +(clusters_y.get(0)-clusters_y.get(1))*(clusters_y.get(0)-clusters_y.get(1))/sigmaY(clusters_y.get(0))/sigmaY(clusters_y.get(1))
-                                       +(clusters_z.get(0)-clusters_z.get(1))*(clusters_z.get(0)-clusters_z.get(1))/sigmaZ(clusters_z.get(0))/sigmaZ(clusters_z.get(1))
-                                       +(clusters_time.get(0)-clusters_time.get(1))*(clusters_time.get(0)-clusters_time.get(1))/sigmaTime(clusters_time.get(0))/sigmaTime(clusters_time.get(1)) );
+                //double distance = sqrt( (clusters_x.get(0)-clusters_x.get(1))*(clusters_x.get(0)-clusters_x.get(1))/sigmaX(clusters_x.get(0))/sigmaX(clusters_x.get(1))
+                //                       +(clusters_y.get(0)-clusters_y.get(1))*(clusters_y.get(0)-clusters_y.get(1))/sigmaY(clusters_y.get(0))/sigmaY(clusters_y.get(1))
+                //                       +(clusters_z.get(0)-clusters_z.get(1))*(clusters_z.get(0)-clusters_z.get(1))/sigmaZ(clusters_z.get(0))/sigmaZ(clusters_z.get(1))
+                //                       +(clusters_time.get(0)-clusters_time.get(1))*(clusters_time.get(0)-clusters_time.get(1))/sigmaTime(clusters_time.get(0))/sigmaTime(clusters_time.get(1)) );
+                
+                double distance = this.calc_distance(clusters_x.get(0), clusters_x.get(1),
+                        clusters_y.get(0), clusters_y.get(1),
+                        clusters_z.get(0), clusters_z.get(1),
+                        clusters_time.get(0), clusters_time.get(1));
                 
                 if(distance < cluster_size_){
                     
@@ -159,9 +173,10 @@ public class ClusterFinder {
             }
             else{
                 //// hierarchiral clustering
+                // put comments:
                 double[] closest_distance = new double[1];
-                int[] subA = new int[1];
-                int[] subB = new int[1];
+                int[] subA = new int[1]; //define:
+                int[] subB = new int[1]; //define:
                 int counter=-1;
                 
                 while(true){
@@ -324,6 +339,11 @@ public class ClusterFinder {
         
     }
     
+    /**
+     * 
+     * @param hits list of hits
+     * @return sorted list by paddle and then energy
+     */
     private ArrayList<AHit> HitsToCluster(List<?> hits) {
         //Sort hits by energy
         ArrayList<AHit> hits2 = (ArrayList<AHit>) hits;
@@ -332,7 +352,17 @@ public class ClusterFinder {
         return (ArrayList<AHit>) hits2;
     }
     
-    
+    /**
+     * define parameters below:
+     * @param begin
+     * @param x
+     * @param y
+     * @param z
+     * @param time
+     * @param subA
+     * @param subB
+     * @param closest_distance 
+     */
     private void find_closest(int begin, ArrayList<Double> x, ArrayList<Double> y, ArrayList<Double> z, ArrayList<Double> time,
                               int[] subA, int[] subB, double[] closest_distance){
         if((begin+1)>=x.size())return;
@@ -340,12 +370,15 @@ public class ClusterFinder {
         
         
         for(int i=begin+1;i<x.size();i++){
-            double distance = sqrt( (x.get(begin)-x.get(i))*(x.get(begin)-x.get(i))/sigmaX(x.get(begin))/sigmaX(x.get(i))
-                                   +(y.get(begin)-y.get(i))*(y.get(begin)-y.get(i))/sigmaY(y.get(begin))/sigmaY(y.get(i))
-                                   +(z.get(begin)-z.get(i))*(z.get(begin)-z.get(i))/sigmaZ(z.get(begin))/sigmaZ(z.get(i))
-                                   +(time.get(begin)-time.get(i))*(time.get(begin)-time.get(i))/sigmaTime(time.get(begin))/sigmaTime(time.get(i)) );
+            //double distance = sqrt( (x.get(begin)-x.get(i))*(x.get(begin)-x.get(i))/sigmaX(x.get(begin))/sigmaX(x.get(i))
+            //                       +(y.get(begin)-y.get(i))*(y.get(begin)-y.get(i))/sigmaY(y.get(begin))/sigmaY(y.get(i))
+            //                       +(z.get(begin)-z.get(i))*(z.get(begin)-z.get(i))/sigmaZ(z.get(begin))/sigmaZ(z.get(i))
+            //                       +(time.get(begin)-time.get(i))*(time.get(begin)-time.get(i))/sigmaTime(time.get(begin))/sigmaTime(time.get(i)) );
             
-            
+            double distance = this.calc_distance(x.get(begin), x.get(i), 
+                    y.get(begin), y.get(i), 
+                    z.get(begin), z.get(i), 
+                    time.get(begin), time.get(i));
             if(distance > cluster_size_){
                 //
                 continue;
@@ -378,6 +411,26 @@ public class ClusterFinder {
       private double sigmaPhi(double phi){ return 1; }
       /// beta = v/c.
       private double sigmaBeta(double beta){ return 1; }
+
+    /**
+     * 
+     * @param x0
+     * @param x1
+     * @param y0
+     * @param y1
+     * @param z0
+     * @param z1
+     * @param t0
+     * @param t1
+     * @return weighted distance taking into account the detector uncertainties
+     */  
+    private double calc_distance(Double x0, Double x1, Double y0, Double y1, Double z0, Double z1, Double t0, Double t1) {
+        return sqrt( (x0-x1)*(x0-x1)/sigmaX(x0)/sigmaX(x1)
+            + (y0-y1)*(y0-y1)/sigmaY(y0)/sigmaY(y1)
+            + (z0-z1)*(z0-z1)/sigmaZ(z0)/sigmaZ(z1)
+            + (t0-t1)*(t0-t1)/sigmaTime(t0)/sigmaTime(t1) );
+
+    }
     
     
 }
